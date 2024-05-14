b91cf84d44faecf008263f64d39aadb02b0dc80a
Deepesh Bhardwaj <150908860+djvampire123@users.noreply.github.com>
2024-05-14 15:02:52 +0530

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -2,8 +2,3 @@ mkdir data
 gdown --fuzzy -O data/data.zip https://drive.google.com/file/d/1vRl6IIX7YbubCyA7ju_7Yl8S27gB9OpV/view
 cd data && unzip data.zip && rm data.zip
 cp -r /content/drive/MyDrive/Underwater/UnderwaterIEAndSR/data/content/drive/MyDrive/Underwater/UnderwaterIEAndSR/data/* /content/drive/MyDrive/Underwater/UnderwaterIEAndSR/data
-<<<<<<< HEAD
-rm -r /content/drive/MyDrive/Underwater/UnderwaterIEAndSR/data/content
-=======
-rm -r /content/drive/MyDrive/Underwater/UnderwaterIEAndSR/data/content
->>>>>>> 5f81d5266265635e3fc9955fa4d56f2fa13d510b
